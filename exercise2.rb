ce261a4598137a0741d6aba906ce46a16b8054ee
Edward Yu <3dward.yu@gmail.com>
2016-08-09 11:52:53 -0400

    Create and complete exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+# 1) use operators and multiply $55 by 15 or 18%
+puts "#{55 * 0.15} for a 15% tip"
+puts "#{55 * 0.18} for an 18% tip"
+
+# 2) ruby returns an error because it cannot combine a fix number and string with + operator
+puts "Hello TeamJohnny" + 5.to_s
+
+# 3)
+puts "#{45628 * 7839}"
+
+=begin 4) Value will be true
+first && statment is false
+second statement is not false (true).
+false || true is true
+=end
+puts (10 < 20 && 30 < 20) || !(10 == 11)
